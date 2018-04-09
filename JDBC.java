cd9441ec0a085005850a2b32b0ad8555c9a3ddbc
Patrick S. Watters <pwatters@ups.com>
2018-04-09 16:02:07 -0400

    corrected "castor" instead of "bingsuns"

--- JDBC.java
+++ JDBC.java
@@ -1,36 +1,62 @@
-import java.sql.DriverManager;
-import java.sql.Connection;
+import java.sql.*;
+import oracle.jdbc.pool.OracleDataSource;
 
 public class JDBC 
 {
-	Connection con = null;
+        public static Connection createConnection()
+        {
+                Connection con = null;
+                try {
+                        DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+                    OracleDataSource ds = new oracle.jdbc.pool.OracleDataSource();
+                ds.setURL("jdbc:oracle:thin:@castor.cc.binghamton.edu:1521:ACAD111");
+                con = ds.getConnection("pwatter1", "pineapple1");
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
+                return con;
+        }
 
-	private String username = "pwatter1";
-	private String password = "pineapple";
-	private String url = "jdbc:oracle:thin:@bingsuns.cc.binghamton.edu:1521:xe";
+        public static void closeConnection(Connection con) 
+        {
+                if (con != null) 
+                { 
+                        try {
+                                con.close();
+                        } catch (Exception e) {
+                                System.err.println("Error: Exception closing jdbc conneciton.");
+                                e.printStackTrace();
+                                System.exit(-1);
+                        }
+                }
+        }
 
-	public static Connection createConnection()
-	{
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver"); //load jdbc driver
-			con = DriverManager.getConnection(url, username, password);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return con;
-	}
+        public static void closeResultSet(ResultSet rs) 
+        {
+                if (rs != null) 
+                { 
+                        try {
+                                rs.close();
+                        } catch (Exception e) {
+                                System.err.println("Error: Exception closing jdbc result set.");
+                                e.printStackTrace();
+                                System.exit(-1);
+                        }
+                }
+        }
 
-	public static void closeConnection() 
-	{
-		if (con != null) 
-		{ 
-			try {
-				con.close();
-			} catch (Exception e) {
-				System.err.println("Error: Exception closing jdbc conneciton.");
-				e.printStackTrace();
-				System.exit(-1);
-			}
-		}
-	}
+        public static void closeStatement(Statement st) 
+        {
+                if (st != null) 
+                { 
+                        try {
+                                st.close();
+                        } catch (Exception e) {
+                                System.err.println("Error: Exception closing jdbc statement.");
+                                e.printStackTrace();
+                                System.exit(-1);
+                        }
+                }
+        }
 }
+
